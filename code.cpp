38407634f8c3e18628623d0204800bb694d53e22
Panna Porri <72212920+pannaporri@users.noreply.github.com>
2021-10-19 20:02:35 +0530

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -10,12 +10,28 @@ int main(){
         cin>>a;
         arr[i]=a;
     }
-    for(int i=k;i<n;i++){
+    if(k==1 && arr[0]==0){
+        cout<<0<<endl;
+    }
+    else if(k>1 && k<n && arr[k]!=0){
+        for(int i=k;i<n;i++){
         if(arr[i]==arr[k]){
             cnt+=1;
         }
     }
     cout<<cnt<<endl;
+    }
+    else if(k==n && arr[k-1]==0){
+        for(int i=0;i<n-1;i++){
+            if(arr[i]>0){
+                cnt+=1;
+            }
+        }
+        cout<<cnt<<endl;
+    }
+    else if(k==n && arr[k-1]!=0){
+        cout<<n<<endl;
+    }
     return 0;
  
 }
